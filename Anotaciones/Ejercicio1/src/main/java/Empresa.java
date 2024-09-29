87c390e256b32b483eede9afb8fb0f602e29def5
Nicolito <jchabra595r@g.educaand.es>
2024-09-29 21:59:49 +0200

    ArrayList

--- Anotaciones/Ejercicio1/src/main/java/Empresa.java
+++ Anotaciones/Ejercicio1/src/main/java/Empresa.java
@@ -1,6 +1,12 @@
+import java.util.ArrayList;
+
 public class Empresa {
 
     private String nombre;
-    private Empleado[] empleado;
+    private ArrayList<Empleado> misEmpleados;
 
+    public Empresa(String nombre) {
+        this.nombre = nombre;
+        this.misEmpleados = new ArrayList<>();
+    }
 }
